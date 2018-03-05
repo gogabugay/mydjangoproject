916fd683295a4c572f9de6524b8aae7ad45fbb76
gogabugay <yuriyroudenko@gmail.com>
2018-03-05 17:04:44 +0300

    db changes and shiet

--- mysite/polls/migrations/0001_initial.py
+++ mysite/polls/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Chioce',
+            name='Choice',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('choice_text', models.CharField(max_length=200)),
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.AddField(
-            model_name='chioce',
+            model_name='choice',
             name='question',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.Question'),
         ),
