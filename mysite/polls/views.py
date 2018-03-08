679e725e3ac470dd28c691bdfc7865405e58a03a
gogabugay <yuriyroudenko@gmail.com>
2018-03-08 22:29:45 +0300

    testin stuff

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name='polls/index.html'
@@ -21,6 +22,8 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name='polls/results.html'
+    def get_queryset(self):
+    	return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
@@ -37,3 +40,7 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def get_queryset(self):
+	return Question.objects.filter(
+		pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
